080c1f0513bfdb60baa63f0cdd4e9633c3baf70a
DPontes <dpontes@protonmail.com>
2022-01-31 16:11:38 +0100

    first commmit

---
+++ soundex.h
@@ -0,0 +1,41 @@
+#ifndef SOUNDEX_H
+#define SOUNDEX_H
+#include <string>
+#include <unordered_map>
+
+static const size_t MaxCodeLength{4};
+
+class Soundex {
+  public:
+    std::string encode(const std::string& word) const {
+      return zeroPad(head(word) + encodeDigits(word));
+    }
+
+  private:
+    std::string head(const std::string& word) const {
+      return word.substr(0, 1);
+    }
+
+    std::string encodeDigits(const std::string& word) const {
+      if (word.length() > 1) return encodedDigit(word[1]);
+      return "";
+    }
+
+    std::string encodedDigit(char letter) const {
+      const std::unordered_map<char, std::string> encodings {
+          {'b', "1"},
+          {'c', "2"},
+          {'d', "3"},
+      };
+
+      return encodings.find(letter)->second;
+    }
+
+    std::string zeroPad(const std::string& word) const {
+      auto zerosNeeded = MaxCodeLength - word.length();
+      return word + std::string(zerosNeeded, '0');
+    }
+
+};
+
+#endif
