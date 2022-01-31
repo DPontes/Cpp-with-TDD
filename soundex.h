5d58475344a223f3cabbf45ea50ea1b345aeb54b
DPontes <dpontes@protonmail.com>
2022-01-31 21:59:46 +0100

    Added test and logic that replaces multiple consonants with digits

--- soundex.h
+++ soundex.h
@@ -8,17 +8,23 @@ static const size_t MaxCodeLength{4};
 class Soundex {
   public:
     std::string encode(const std::string& word) const {
-      return zeroPad(head(word) + encodeDigits(word));
+      return zeroPad(head(word) + encodedDigits(tail(word)));
     }
 
   private:
+    std::string tail(const std::string& word) const {
+      return word.substr(1);
+    }
+
     std::string head(const std::string& word) const {
       return word.substr(0, 1);
     }
 
-    std::string encodeDigits(const std::string& word) const {
-      if (word.length() > 1) return encodedDigit(word[1]);
-      return "";
+    std::string encodedDigits(const std::string& word) const {
+      std::string enconding;
+
+      for (auto letter : word) enconding += encodedDigit(letter);
+      return enconding;
     }
 
     std::string encodedDigit(char letter) const {
